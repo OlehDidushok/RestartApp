8489650be908ae9fda730cd604de0c6c9242485d
OlehDidushok <didushokov@gmail.com>
2024-01-19 22:06:07 +0200

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Oleh on 19.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
